5ff9a7c0ba667ae580a30614948c79bd03341f7c
Mark <zagumennikovmark@gmail.com>
2023-01-25 16:29:18 +0500

    GOVNOFORSTATS

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express'
-const app=express()
+import dotenv from 'dotenv'
+import {connect} from './public/backend/connector.js'
 
-app.get('/',(req,res)=>res.send("seerver photos"))
-app.listen(5000)
+const app=express()
+app.use(express.static('public'))
+dotenv.config()
+
+app.get('/',(req,res)=>{
+    connect()
+    res.send("server photos")
+})
+app.listen(3000)
